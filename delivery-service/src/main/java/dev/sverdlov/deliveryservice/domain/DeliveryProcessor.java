b2ba56c96ff224e6bd71a5a45e5edb835bb3b716
Miracle- <abed90eg@gmail.com>
2026-07-05 15:40:37 +0300

    feat(delivery): implement event-driven delivery service

---
+++ delivery-service/src/main/java/dev/sverdlov/deliveryservice/domain/DeliveryProcessor.java
@@ -0,0 +1,59 @@
+package dev.sverdlov.deliveryservice.domain;
+
+import dev.sverdlov.api.kafka.DeliveryAssignedEvent;
+import dev.sverdlov.api.kafka.OrderPaidEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class DeliveryProcessor {
+
+    private final DeliveryEntityRepository repository;
+    private final KafkaTemplate<Long, DeliveryAssignedEvent> kafkaTemplate;
+
+    @Value("${delivery-assigned-topic}")
+    private String deliveryAssignedTopic;
+
+    public void processOrderPaid(OrderPaidEvent event) {
+        var orderId = event.orderId();
+        var found = repository.findByOrderId(orderId);
+        if (found.isPresent()) {
+            log.info("Found order delivery was already assigned: delivery={}", found.get());
+            return;
+        }
+
+        var assignedDelivery = assignDelivery(orderId);
+        sendDeliveryAssignedEvent(assignedDelivery);
+    }
+
+    private DeliveryEntity assignDelivery(Long orderId) {
+        var entity = new DeliveryEntity();
+        entity.setOrderId(orderId);
+        entity.setCourierName("courier-" + ThreadLocalRandom.current().nextInt(100));
+        entity.setEtaMinutes(ThreadLocalRandom.current().nextInt(10, 45));
+        log.info("saved order delivery was assigned: delivery={}", entity);
+
+        return repository.save(entity);
+    }
+
+    private void sendDeliveryAssignedEvent(DeliveryEntity assignedDelivery) {
+        kafkaTemplate.send(
+                deliveryAssignedTopic,
+                assignedDelivery.getOrderId(),
+                DeliveryAssignedEvent.builder()
+                        .courierName(assignedDelivery.getCourierName())
+                        .orderId(assignedDelivery.getOrderId())
+                        .etaMinutes(assignedDelivery.getEtaMinutes())
+                        .build()
+        ).thenAccept(result -> {
+            log.info("Delivery assigned event sent: deliveryId={}", assignedDelivery.getId());
+        });
+    }
+}
